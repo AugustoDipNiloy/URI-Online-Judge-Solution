9921f7df091327847c65604df18807b7b8352492
Augusto Dip Niloy <69030254+AugustoDipNiloy@users.noreply.github.com>
2020-08-01 15:14:04 +0600

    Create 1010.py

---
+++ 1010.py
@@ -0,0 +1,11 @@
+a, b, c = input().split(" ")
+a=int(a)
+b=int(b)
+c=float(c)
+d=b*c
+a, b, c = input().split(" ")
+a=int(a)
+b=int(b)
+c=float(c)
+d=d+b*c
+print("VALOR A PAGAR: R$ {:.2f}".format(d))
